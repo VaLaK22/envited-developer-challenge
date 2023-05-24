3260b549a9a54f96d4b3b73b79f1a6e8c1f023c5
VaLaK22 <mohammadzughni@gmail.com>
2023-05-24 12:19:52 +0200

    init

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -5,7 +5,7 @@ import { sendEmailToken } from "../services/emailService";
 require("dotenv").config();
 
 const EMAIL_TOKEN_EXPIRATION_MINUTES = 10;
-const AUTHENTICATION_EXPIRATION_HOURS = 12;
+const AUTHENTICATION_EXPIRATION_HOURS = 24 * 30;
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
 const router = Router();
